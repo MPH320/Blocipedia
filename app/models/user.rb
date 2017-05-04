c05d7c2b0bf8a48f674f1f9dcec33f2811357eeb
Michelangelo Pereira <michelangelo.ph@gmail.com>
2017-05-04 17:55:10 +0000

    Favicon added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   ##
-  
+   #make a favicon, test
+  #check where rails sets defaul port and make sure it allows to check for heroku port
+  #download locally, install git/heroku, push, test
+  #research c9 disc space, maybe install linux and run from there or upgrade plan
+  #download another student's rails project and see if you can host, https://github.com/kvoshell/Bloccit
+  #check digital ocean
   has_many :wikis
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
